921dd5130ada7da40ba5e66c37456bffea41de74
Zac <zcupec@gmail.com>
2021-04-26 10:28:33 -0400

    Loading the ROM
    Loads the Rom into pyboy

---
+++ Metroid.py
@@ -0,0 +1,15 @@
+from pyboy import PyBoy
+
+pyboy = PyBoy("Metroid II - Return of Samus.gb")
+while not pyboy.tick():
+    '''
+    # TODO:
+    1. Get input from PyBoy
+    2. Create Neural Net
+    3. Send output to PyBoy
+    
+    ------------------------
+    Outputs: Up, Down, Left, Right, A, B, 
+    
+    '''
+    pass
